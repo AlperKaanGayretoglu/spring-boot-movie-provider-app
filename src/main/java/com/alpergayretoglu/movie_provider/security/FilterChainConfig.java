b83dc030104d26565e209f82f938386d59ed203e
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-03-26 19:02:55 +0300

    Created the security package and created these classes: FilterChainConfig, JwtAuthenticationFilter, JwtService, SecurityConfig

---
+++ src/main/java/com/alpergayretoglu/movie_provider/security/FilterChainConfig.java
@@ -0,0 +1,41 @@
+package com.alpergayretoglu.movie_provider.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class FilterChainConfig {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/register").permitAll()
+                .requestMatchers("/swagger-ui/index.html").permitAll()  // OpenAPI
+                .requestMatchers("/v3/api-docs").permitAll()            // OpenAPI
+                .anyRequest().permitAll() //.authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
