b08c2be67439a336683607da3138346d3d4a6621
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-03-26 15:36:38 +0300

    Initial Commit

---
+++ src/main/java/com/alpergayretoglu/movie_provider/MovieProviderApplication.java
@@ -0,0 +1,13 @@
+package com.alpergayretoglu.movie_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieProviderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alpergayretoglu/movie_provider/MovieProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpergayretoglu.movie_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
