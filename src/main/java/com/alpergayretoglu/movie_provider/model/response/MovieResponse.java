759317ad9742f4bc03f289123cd88c979b52bafd
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-04-21 18:09:06 +0300

    Modified MovieResponse accordingly:
    Turn the categories object from a List to a Set

--- src/main/java/com/alpergayretoglu/movie_provider/model/response/MovieResponse.java
+++ src/main/java/com/alpergayretoglu/movie_provider/model/response/MovieResponse.java
@@ -5,19 +5,20 @@ import com.alpergayretoglu.movie_provider.model.entity.Movie;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @AllArgsConstructor
 @Data
 public class MovieResponse {
 
     private String title;
-    private List<String> categories;
+    private Set<String> categories;
 
     public static MovieResponse fromEntity(Movie movie) {
         return new MovieResponse(
                 movie.getTitle(),
-                movie.getCategories().stream().map(Category::getName).toList()
+                new HashSet<>(movie.getCategories().stream().map(Category::getName).toList())
         );
     }
 }
