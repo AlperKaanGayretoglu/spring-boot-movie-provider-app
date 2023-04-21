7dfddc53c664fae33d3e3156960815659113042e
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-04-21 18:08:31 +0300

    Modified UserRequests accordingly:
    Removed UserRole from the requests entirely since it shouldn't be possible to change the UserRole with a requets

--- src/main/java/com/alpergayretoglu/movie_provider/model/request/user/UserUpdateRequest.java
+++ src/main/java/com/alpergayretoglu/movie_provider/model/request/user/UserUpdateRequest.java
@@ -2,7 +2,6 @@ package com.alpergayretoglu.movie_provider.model.request.user;
 
 import com.alpergayretoglu.movie_provider.constants.ApplicationConstants;
 import com.alpergayretoglu.movie_provider.model.entity.User;
-import com.alpergayretoglu.movie_provider.model.enums.UserRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -27,16 +26,12 @@ public class UserUpdateRequest {
     @Size(min = ApplicationConstants.PASSWORD_MIN_LENGTH, max = ApplicationConstants.PASSWORD_MAX_LENGTH)
     private String password;
 
-    @NotBlank
-    private UserRole role;
-
     public static User toEntity(UserUpdateRequest request) {
         return User.builder()
                 .name(request.getName())
                 .surname(request.getSurname())
                 .email(request.getEmail())
                 .password(request.getPassword())
-                .role(request.getRole())
                 .build();
     }
 }
